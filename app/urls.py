caa6a2bec5efc689dfde5c00c22c8f31df99ae2f
Prince Agarwal <prince.agarwal@codemastr.com>
2020-08-17 10:30:58 +0530

    make it better

--- app/urls.py
+++ app/urls.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 from django.conf import settings
 from django.urls import path, include
 from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from . import settings
 
 from main.views import IndexPageView
 
@@ -15,5 +18,5 @@ urlpatterns = [
     path('accounts/', include('accounts.urls')),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
