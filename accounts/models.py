21eed9d9cbcd7a82004b4450cecf5e769675948e
Prince Agarwal <prince.agarwal@codemastr.com>
2020-08-17 10:10:31 +0530

    make it better

--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import User
-from phonenumber_field.modelfields import PhoneNumberField
 from django.core.validators import MaxValueValidator,MinValueValidator
 
 
