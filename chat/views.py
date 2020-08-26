aee49810c804e60adb0e9f7ccbfd58416a35b10c
Prince Agarwal <prince.agarwal@codemastr.com>
2020-08-26 19:48:07 +0530

    make it better

--- chat/views.py
+++ chat/views.py
@@ -15,21 +15,37 @@ def index(request):
 
 
 def room(request, room_name):
-    sender = User.objects.get(username = room_name)
-    chats = Chat.objects.filter(Q(sender__username__icontains=sender.username) | Q(reciver__username__contains=sender.username))
-    old_chat = ""
-    for chat in chats[:10]:
-        old_chat = old_chat + " \n " +  chat.messages + "\n" + str(chat.created_at)
-    chats = Chat.objects.filter(reciver = sender)
-    for chat in chats[:10]:
-        old_chat = old_chat + " \n " +  chat.messages + "\n" + str(chat.created_at)
-    return render(request, 'chat/room.html', {
-        "old_chats": chats,
-        'room_name': room_name
-    })
+    try:
+        sender = User.objects.get(username = room_name)
+        chats = Chat.objects.filter(Q(sender__username__icontains=sender.username) | Q(reciver__username__contains=sender.username))
+        old_chat = ""
+        for chat in chats[:10]:
+            old_chat = old_chat + " \n " +  chat.messages + "\n" + str(chat.created_at)
+        chats = Chat.objects.filter(reciver = sender)
+        for chat in chats[:10]:
+            old_chat = old_chat + " \n " +  chat.messages + "\n" + str(chat.created_at)
+        return render(request, 'chat/room.html', {
+            "old_chats": [],
+            'room_name': room_name
+        })
+    except User.DoesNotExist:
+        User_list  = User.objects.all()
+        return render(request, 'chat/room.html', {
+            "no_found": True,
+            'room_name': room_name,
+            'user_list': list(User_list)
+        })
+    except:
+        User_list  = User.objects.all()
+        return render(request, 'chat/room.html', {
+            "no_found": True,
+            'room_name': room_name,
+            'user_list': list(User_list)
+        })
+        
+
 def users_available(request):
     User_list  = User.objects.all()
-    print(User_list)
     return render(request, 'chat/list_user.html', {
         'user_list': list(User_list)
     })
